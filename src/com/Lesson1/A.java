241dd4c5f319b79f785310a006570d5a03f4c7cf
TKKCODE <thommandrukirankumar2@gmail.com>
2023-03-24 21:07:17 +0530

    Project Folder Done

---
+++ src/com/Lesson1/A.java
@@ -0,0 +1,7 @@
+package com.Lesson1;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("From Lesson1 A.java");
+    }
+}
---
+++ src/com/Lesson2/B.java
@@ -0,0 +1,7 @@
+package com.Lesson2;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println("From Lesson2 B.java");
+    }
+}
